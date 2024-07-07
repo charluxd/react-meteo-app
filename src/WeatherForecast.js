2e518ff1cfc3f89d8ff12f1ed8cd51051c1b8cb8
Charlotte Lee <charlotteuxd@gmail.com>
2024-07-07 17:34:42 +0800

    Added the API

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -3,6 +3,13 @@ import WeatherIcon from './WeatherIcon';
 import './WeatherForecast.css';
 
 function WeatherForecast() {
+  let apiKey = 'ca80fb7d3o48t3c14460b13a3d83ca48';
+  let longtitude = '103.8519072';
+  let lattitude = '1.2899175';
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lattitude}&lon=${longtitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
